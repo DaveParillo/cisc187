cf071bb49b6d864018e8514a21ce7c9c91f70504
DaveParillo <DaveParillo@users.noreply.github.com>
2016-12-03 21:14:38 -0800

    changed user

--- backup.sh
+++ backup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 tar cyf solutions.zip --exclude .DS_Store ./solutions
-gpg --output solutions.gpg --encrypt --armor --recipient dave.parillo@gmail.com solutions.zip
+gpg --output solutions.gpg --encrypt --armor --recipient dparillo@sdccd.edu solutions.zip
 rm solutions.zip
 
 tar cyf quiz.zip --exclude .DS_Store ./quiz
-gpg --output quiz.gpg --encrypt --armor --recipient dave.parillo@gmail.com quiz.zip
+gpg --output quiz.gpg --encrypt --armor --recipient dparillo@sdccd.edu quiz.zip
 rm quiz.zip
 
