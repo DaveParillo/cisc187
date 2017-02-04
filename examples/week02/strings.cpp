fac84ac65516070308ea16d0dd8af14102833f08
DaveParillo <DaveParillo@users.noreply.github.com>
2017-02-03 17:58:23 -0800

    reorganized week 2 slides

--- examples/week02/strings.cpp
+++ examples/week02/strings.cpp
@@ -4,17 +4,17 @@
 int main()
 {
   std::string a = "hello";
-	a += ", world!";   // joining strings is pretty easy
+  a += ", world!";   // joining strings is pretty easy
 
   std::string b = a;      // so is copying one string into another
 
-	if (a == b) {
-		b[0] = 'H';      // and a string feels like an 'array of char' 
-		b[7] = 'W';
-	}
+  if (a == b) {
+    b[0] = 'H';      // and a string feels like an 'array of char' 
+    b[7] = 'W';
+  }
 
   std::cout << a << '\n';
   std::cout << b << '\n';
 
-	return 0;
+  return 0;
 }
