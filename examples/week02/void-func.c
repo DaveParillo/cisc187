1b3dcb09a7caa7f67b237312327a41e1a5613b0a
DaveParillo <DaveParillo@users.noreply.github.com>
2017-02-11 10:19:52 -0800

    clarified assigned reading for week 4, added examples

---
+++ examples/week02/void-func.c
@@ -0,0 +1,42 @@
+// In C, it is legal to define a function that defines no parameter list
+// This is not the same as func(void), which explicitly declares no parameters are passed.
+//
+// Not using void means a function that takes an unspecified (but not variable) 
+// number of parameters.
+//
+// This example compiles and runs, but makes no sense.
+// You have been warned.
+#include<stdio.h>
+
+const double PI = 3.14159265358979323846;
+
+float area1(float);
+float area2(void);
+float area3();
+
+
+
+int main() {
+  int height = 3;
+  float width = 5.0f;
+
+  printf("\nArea 1 : %.2f\n", area1(2.0f));
+  printf("\nArea 2 : %.2f\n", area2());
+  printf("\nArea 3 : %.2f\n", area3(height, width));
+
+  return (0);
+}
+
+float area1 (float radius) {
+  return PI * radius * radius;
+}
+
+float area2 (void) {
+  return 1;
+}
+
+float area3 () {
+  return area1(3);
+}
+
+
---
+++ examples/week04/05-function-pointer-passing.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <string>
+
+typedef int (*op)(int x, int y);
+
+int add     (int x, int y);
+int multiply(int x, int y);
+int subtract(int x, int y);
+int divide  (int x, int y);
+
+op  get_command(const char& command);
+int calculate(const op operation, int x, int y);
+
+int main()
+{
+  std::string op;
+
+  int x = 10;
+  int y = 2;
+
+  int (*pf)(int x, int y);
+
+  while (true)
+  {
+    std::cout << "Operation: ";
+    std::cin >> op;
+    if (op[0] == 'q') break;
+
+    pf = get_command(op[0]);
+
+    std::cout << calculate(pf, x,  y) << std::endl;
+
+  }
+
+  return 0;
+}
+
+op get_command(const char& command) {
+  op pf = nullptr;         // pointer to a function to execute on all operands
+  switch (command) {
+    case 'a':
+      pf = add;
+      break;
+    case 'm':
+      pf = multiply;
+      break;
+    case 's':
+      pf = subtract;
+      break;
+    case 'd':
+      pf = divide;
+      break;
+    default:
+      pf = add;
+      std::cout << "input unrecognized.  defaulting to 'add'\n";
+      break;
+  }
+  return pf;
+
+}
+
+int calculate(const op operation, int x, int y) {
+  return (*operation)(x,  y);
+}
+
+
+int add(int x, int y)
+{
+  return x + y;
+}
+
+int multiply(int x, int y)
+{
+  return x * y;
+}
+
+int subtract(int x, int y)
+{
+  return x - y;
+}
+
+int divide(int x, int y)
+{
+  return x / y;
+}
