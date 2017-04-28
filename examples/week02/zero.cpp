8a6247e9bf0dc3dd5e7ec984bb4b38d02cff7150
DaveParillo <DaveParillo@users.noreply.github.com>
2017-04-27 18:50:34 -0700

    added ones compleement table

--- examples/week02/zero.cpp
+++ examples/week02/zero.cpp
@@ -6,7 +6,12 @@
 // and
 // http://www.volkerschatz.com/science/float.html
 //
+#include <bitset>
 #include <iostream>
+
+using std::cout;
+using std::bitset;
+
 
 int main() {
 
@@ -16,18 +21,25 @@ int main() {
   //   2. No inconsistency representing 0
   int x = -1;
   int y = ~x;  // one's complement of x
-  int z = x * -0;
+  int z = x * 0;
 
-  std::cout << "x: " << x << '\n';
-  std::cout << "y: " << y << '\n';
-  std::cout << "z: " << z << '\n';
+  cout << "x: " << x << "\t(" << bitset<4>(x) << ")\n";
+  cout << "y: " << y << "\t(" << bitset<4>(y) << ")\n";
+  cout << "z: " << z << "\t(" << bitset<4>(z) << ")\n";
+  cout << "~z: " << ~z << "\t(" << bitset<4>(~z) << ")\n";
 
   // The IEEE standard for floating point numbers requires both +0 and −0
   double u = -1.0;
   double v = u * 0.0;
-  double w = ~u;
+  double w = v * -1.0;
 
-  std::cout << "u: " << u << '\n';
-  std::cout << "v: " << v << '\n';
-  std::cout << "w: " << w << '\n';
+  cout << "u: " << u << '\n';
+  cout << "v: " << v << '\n';
+  cout << "w: " << w << '\n';
+
+  cout << " value \t binary \tone's complement \n";
+  cout << " ===== \t ====== \t================ \n";
+  for (int i = -4; i< 8; ++i) {
+    cout << "  " << i << '\t' << bitset<4>(i) << "\t\t" << ~i << '\t' << bitset<4>(~i) << '\n';
+  }
 }
