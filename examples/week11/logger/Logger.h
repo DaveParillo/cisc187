d0107436cd61397f4db716134c186e6b2b9af7f8
DaveParillo <DaveParillo@noreply.github.com>
2017-05-17 21:57:26 -0700

    added more complex singleton: logger example

---
+++ examples/week11/logger/Logger.h
@@ -0,0 +1,103 @@
+#pragma once
+
+#include <map>
+#include <string>
+
+namespace mesa
+{
+  /**
+   * Defines the level of detail of a message.
+   * 
+   * Every message must define a single LogLevel.
+   * The log level controls when the message is printed.
+   * Messages equal to or greater in severity to the current log level are printed.
+   */
+  enum class LogLevel 
+  {
+    /** 
+     * The most detailed log level.  Use for function parameter logging, return values, etc.
+     */
+    TRACE,
+    /** 
+     * A detailed log level.  Use for items you don't want to see logged often.
+     */
+    DEBUG,
+    /** 
+     * A general log level.  The default level.
+     */
+    INFO,
+    /** 
+     * Warnings.  Things that may impact correct output.
+     */
+    WARN,
+    /** 
+     * Errors.  Errors are unexpected and nearly always mean our results are incorrect.
+     */
+    ERROR,
+    /** 
+     * If we get this, we expect the program to exit prematurely.
+     */
+    FATAL,
+    /**
+     * Not an severity level, just something we *always* want to see in the log.
+     */
+    ALWAYS
+  };
+
+    
+  /**
+   * A simple console logger.
+   * 
+   */
+  class Logger {
+    public:
+      static Logger* instance();
+
+
+      /**
+       * Change the logging threshold.
+       * @param log_level the desired minimum logging level.
+       */
+      void level (const std::string& log_level);
+      /**
+       * Change the logging threshold.
+       * @param log_level the desired minimum logging level.
+       */
+      void level (const LogLevel log_level) { _log_level = log_level;}
+      /**
+       * Get the logging threshold.
+       * @return the system logging threshold.
+       */
+      LogLevel level () const {return _log_level;}
+      /**
+       * Log a message at a specified importance.
+       * @param log_level the importance of this message.
+       * @param message the text to log.
+       */
+      void at    (const LogLevel& log_level, const std::string& message) const;
+
+      
+    private:
+      Logger() = default;
+      static Logger* _instance;
+      LogLevel     _log_level;    /**< The logging threshold. */
+
+
+  };
+
+
+
+  /**
+   * Allows the LogLevel enum class members to be printed with meaningful strings.
+   * Example:
+   *
+   *     cout << "Level is " << level_to_string(LogLevel::FATAL) << endl;
+   * @param log_level the LogLevel to be converted
+   * @return the std::string representation of the LogLevel
+   */
+  const std::string& level_to_string(LogLevel log_level);
+
+
+}
+
+
---
+++ examples/week11/logger/main.cpp
@@ -0,0 +1,64 @@
+#include <cctype>
+#include <iostream>
+#include <string>
+#include <unistd.h>
+
+#include "Logger.h"
+
+/**
+ * General mesa namespace.
+ */
+namespace mesa {
+
+  static void show_usage(const std::string name)
+  {
+    std::cerr << "Usage: " << name << " [-h] [-l log_level] -f <file name>\n"
+      << "Options:\n"
+      << "  -h   Show this text\n\n"
+      << "  -l   Optional. Set the log detail level.\n"
+      << "       The default is 'info'\n\n"
+      << "  -f   Required. File name containing commands for the sequencer\n"
+      << "       The file should be plain text, 1 command per line\n"
+      << "       If no file is specified, the program will exit.\n\n"
+      << std::endl;
+  }
+}
+
+int main(int argc, char** argv) {
+  using namespace mesa;
+
+  auto cmd = 0;
+  std::string fname;
+
+  Logger* log = Logger::instance();
+  log->level("INFO");
+
+
+  while ((cmd = getopt (argc, argv, "hf:l:")) != -1) {
+    switch (cmd) {
+      case 'f':
+        fname = optarg;
+        break;
+      case 'l':
+        log->level(optarg);
+        break;
+      case 'h':
+      default:
+        show_usage(argv[0]);
+        return 1;
+    }
+  }
+
+  if (fname.empty()) {
+    log->at(LogLevel::FATAL, "No input file specified!  Exiting.");
+    show_usage(argv[0]);
+    return -1;
+  }
+
+  log->at(LogLevel::INFO, "program complete.");
+
+  delete log;
+  return 0;
+}
+
+
