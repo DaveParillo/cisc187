b7063e0c2f23dc42a6aaee8a7eb6b12e06b5aeb4
DaveParillo <DaveParillo@noreply.github.com>
2017-08-30 21:05:08 -0700

    added string literal to example

--- examples/week02/strings.cpp
+++ examples/week02/strings.cpp
@@ -1,20 +1,29 @@
 #include <string>
 #include <iostream>
 
+using std::string;
+
 int main()
 {
-  std::string a = "hello";
-  a += ", world!";   // joining strings is pretty easy
+  string a = "hello";
+  a += ", world!";        // joining strings is pretty easy
 
-  std::string b = a;      // so is copying one string into another
+  string b = a;           // so is copying one string into another
 
   if (a == b) {
-    b[0] = 'H';      // and a string feels like an 'array of char' 
+    b[0] = 'H';           // and a string feels like an 'array of char' 
     b[7] = 'W';
   }
 
   std::cout << a << '\n';
   std::cout << b << '\n';
 
-  return 0;
+  // one common point of confusion: 
+  auto x = "Goodbye";          // x is NOT a string: it is const char*
+  //auto y = "everyone"s;      // y defines a string literal in C++14
+  auto y = string{"everyone"}; // y is a std::string
+
+  std::cout << x << ", " << y << '\n';
+
 }
+     
