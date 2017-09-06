65dc1e148186b5bc90a2bb574b8231d673d3c3e1
DaveParillo <DaveParillo@users.noreply.github.com>
2017-09-05 20:28:47 -0700

    clarified type traits example

--- examples/week03/08-type-traits.cpp
+++ examples/week03/08-type-traits.cpp
@@ -2,10 +2,13 @@
 #include <type_traits>
 
 int main() {
+  std::cout << std::boolalpha;
+
   if(std::is_signed<char>::value){
-    std::cout << std::is_same<char, signed char>::value;
+    std::cout << "char is signed, is it same? " << std::is_same<char, signed char>::value;
   }else{
-    std::cout << std::is_same<char, unsigned char>::value;
+    std::cout << "char is unsigned, is it same? " << std::is_same<char, unsigned char>::value;
   }
+  std::cout << std::endl;
 }
 
