4cd9c441bfe0fbb8963331703ac7e828b2f9d7da
DaveParillo <DaveParillo@noreply.github.com>
2017-09-20 10:02:30 -0700

    cleaned up week 2 examples, added echo example

---
+++ examples/week02/echo.cpp
@@ -0,0 +1,78 @@
+//
+// A simple program to echo standard input to standard out
+// Funnky options:
+//  - Program can repeat the number of times to echo input
+//  - Program can run interactively and echo input received 1 line at a time
+//
+#include <cstring>
+#include <iostream>
+#include <string>
+
+/**
+ * Display a usage statement for this program.
+ */
+static void usage(const char* name);
+/**
+ * Explain how to use this program
+ */
+static void help (const char* name);
+
+/**
+ * Parse command line and return the repeat count.
+ */
+unsigned process_args(int argc, char** argv);
+
+/**
+ * Echo input received 'repeat' number of times.
+ */
+int echo (unsigned repeat);
+
+
+int main(int argc, char** argv) {
+  auto repeat = process_args(argc, argv);
+  return echo(repeat);
+}
+
+
+static void usage(const char* name) {
+  std::cerr << "Usage: " << name << " [-h] [-r #repeats]\n";
+}
+
+static void help (const char* name) {
+  usage(name);
+  std::cerr << "Options:\n"
+    << "  -h   Show this text\n"
+    << "  -r   Set the number of times to repeat the text.  Default = 1.\n"
+    << std::endl;
+  exit(0);
+}
+
+unsigned process_args(int argc, char** argv) {
+  unsigned repeat = 1;
+  for (int i=1; i < argc; ++i) {
+    if (!std::strncmp(argv[i], "-h", 2)) {
+      help(argv[0]);
+    } else if (!std::strncmp(argv[i], "-r", 2)) {
+      ++i;
+      if (i < argc) {
+        repeat = atoi(argv[i]);
+      } else {
+        std::cerr << "Error using '-r' argument: no repeat spacified\n";
+        usage(argv[0]);
+      }
+
+    }
+  }
+  return repeat;
+}
+
+int echo (unsigned repeat) {
+  std::string line;
+  while (getline(std::cin, line)) {
+    for (auto i = 0ul; i < repeat; ++i) {
+      std::cout << line << '\n'; // getline strips newline
+    }
+   line.clear();
+  }
+  return 0;
+}
--- examples/week02/passing-parameters.cpp
+++ examples/week02/passing-parameters.cpp
@@ -1,4 +1,5 @@
 
+#include <cassert>
 #include <iostream>
 
 /**
@@ -7,7 +8,9 @@
  */
 void by_value(int x) 
 {
+  assert (x == 11);
   x = 99;
+  assert (x == 99);
 }
 
 /**
@@ -18,8 +21,10 @@ void by_value(int x)
  */
 void by_reference (int& x)
 {
+  assert (x == 11);
   std::cout << "in by_ref the address of x is   " << &x << '\n';
   x = -1;
+  assert (x == -1);
 }
 
 int main ()
@@ -30,8 +35,10 @@ int main ()
   std::cout << "in main the address of alpha is " << &alpha << '\n';
   std::cout << "in main the address of beta is  " << &beta << '\n';
 
-  by_value(alpha);
-  by_reference(beta);
+  by_value (alpha);
+  by_reference (beta);
+  assert (alpha == 11);
+  assert (beta  == -1);
 
   std::cout << "alpha is now " << alpha << '\n';
   std::cout << "beta is now " << beta << '\n';
