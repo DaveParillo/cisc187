a58420c7eea88f290c280d33c8295823e809fa19
DaveParillo <DaveParillo@users.noreply.github.com>
2017-10-11 22:28:11 -0700

    moved static out of class

--- examples/week09/oo-shipping/EvenBetterShipping.h
+++ examples/week09/oo-shipping/EvenBetterShipping.h
@@ -3,6 +3,8 @@
 #include <algorithm>
 #include <string>
 
+static constexpr int min_weight = 1;
+
 /** 
  * A class with simple encapsulation 
  */
@@ -24,7 +26,6 @@ class EvenBetterShipping {
       return address_;
     }
   private:
-    static constexpr int min_weight = 1;
     int weight_ = 2;      // in pounds
     int distance_ = 100;    // in miles
     std::string address_ = "My mom's house";
