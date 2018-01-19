0a6c2cbffb91f2f90eaabbdd3dfbf23a34d561e9
DaveParillo <DaveParillo@noreply.github.com>
2018-01-19 11:44:10 -0800

    fixed undefined behavior error

--- examples/week04/06-jump-table.cpp
+++ examples/week04/06-jump-table.cpp
@@ -2,11 +2,7 @@
 #include <string>
 using std::cout;
 
-// could these be std:: strings? yes.
-// or just a char? yes.
-// but we are in pointers week, so we're going a bit 'old school'
-
-const char* prompt();
+std::string prompt();
 int jump(const char*);
 
 void a() { cout << "you chose a()\n"; }
@@ -18,14 +14,14 @@ void c() { cout << "you chose c()\n"; }
 void (*functions[])() = { a, b, c, nullptr };
 
 int main() {
-  while(jump(prompt()))        // loop until we get a 0 from jump
-    ;                          // the semi-colon is on a separate line to 
-                               // silence warnings about a missing loop body
+  while(jump(prompt().c_str()))  // loop until we get a 0 from jump
+    ;                            // the semi-colon is on a separate line to 
+                                 // silence warnings about a missing loop body
   cout << "\nDone.\n";
   return 0;
 }
 
-const char* prompt () {
+std::string prompt () {
   cout << "Choose an option:\n";
   cout << "1. Function a\n";
   cout << "2. Function b\n";
@@ -35,8 +31,7 @@ const char* prompt () {
 
   std::string opt;
   std::cin >> opt;
-  return opt.c_str();  // convert our std::string to const char *
-                       // since C++11 data() == c_str()
+  return opt;
 }
 
 int jump (const char* rs) {
