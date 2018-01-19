48f50a7aeeb542370e9eb69bc6fcc4183a013632
DaveParillo <DaveParillo@noreply.github.com>
2018-01-19 10:27:36 -0800

    reduced scope of char c

--- examples/week01/asciitable.cpp
+++ examples/week01/asciitable.cpp
@@ -7,10 +7,9 @@ using std::dec;
 
 int main() {
   std::string heading = "\ndec\toct\thex\tchar\n";
-  char c = 0;
   for (int i = 0; i < 128; ++i) {
     if (!(i%20)) cout << heading;
-    c = i;
+    char c = i;
     // the printable characters are between hex 20 and 7e
     cout << dec << i << '\t'
          << oct << i << '\t'
