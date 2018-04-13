d62c75a079781c241d5ec89e6973b798b67c9777
DaveParillo <DaveParillo@noreply.github.com>
2018-04-13 12:04:31 -0700

    added tests

--- labs/test_cases/lab10b/step2.cpp
+++ labs/test_cases/lab10b/step2.cpp
@@ -16,6 +16,20 @@ SCENARIO( "Add the digits of two vectors") {
       CHECK (actual.size() == expected.size());
     }
     AND_THEN( "the result should be 5" ) {
+      REQUIRE(actual.back() == expected.back());
+    }
+  }
+
+  GIVEN( "x = {1,2} and y = {3,4}") {
+    std::vector<int> x   {1,2};
+    std::vector<int> y   {3,4};
+
+    std::vector<int> actual = mesa::add(x, y);
+    std::vector<int> expected = {4,6};
+    THEN( "each digit should be in a separate element of the vector" ) {
+      CHECK (actual.size() == expected.size());
+    }
+    AND_THEN( "the result should be 46" ) {
       for (unsigned i = 0; i < expected.size(); ++i) {
         CAPTURE (i);
         REQUIRE(actual[i] == expected[i]);
@@ -23,7 +37,7 @@ SCENARIO( "Add the digits of two vectors") {
     }
   }
 
-  GIVEN( "x = {9} and y = {3}") {
+  GIVEN( "x = {0,9} and y = {0,3}") {
     std::vector<int> x   {0,9};
     std::vector<int> y   {0,3};
 
@@ -40,24 +54,38 @@ SCENARIO( "Add the digits of two vectors") {
     }
   }
 
-/*
+  GIVEN( "x = {9,9} and y = {1}") {
+    std::vector<int> x   {9,9};
+    std::vector<int> y   {1};
+
+    std::vector<int> actual = mesa::add(x, y);
+    std::vector<int> expected = {1,0,0};
+    THEN( "each digit should be in a separate element of the vector" ) {
+      CHECK (actual.size() == expected.size());
+    }
+    AND_THEN( "the result should be 100" ) {
+      for (unsigned i = 0; i < expected.size(); ++i) {
+        CAPTURE (i);
+        REQUIRE(actual[i] == expected[i]);
+      }
+    }
+  }
 
   GIVEN( "the two example vectors from the lab") {
-    std::vector<int> x   {0,0,9,9,0,1,2,3,4,5,6,7,8,9};
+    std::vector<int> x   {9,9,0,1,2,3,4,5,6,7,8,9};
     std::vector<int> y         {9,8,7,6,5,4,3,2,1,9};
 
     std::vector<int> actual = mesa::add(x, y);
     std::vector<int> expected = {1,0,0,0,0,0,0,0,0,0,0,0,8};
     THEN( "the leading 0's should be removed" ) {
-      CHECK (actual.size() == x.size());
+      CHECK (actual.size() == expected.size());
     }
     AND_THEN( "the result should be 1000000000008" ) {
-      for (unsigned i = 0; i < actual.size(); ++i) {
+      for (unsigned i = 0; i < expected.size(); ++i) {
         CAPTURE (i);
         CHECK(actual[i] == expected[i]);
       }
     }
   }
-*/
 }
 
