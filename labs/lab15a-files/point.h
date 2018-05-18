0ba7d3bdcecb5bec46ace8d139241558b6af2128
DaveParillo <DaveParillo@noreply.github.com>
2018-05-18 16:49:32 -0700

    tweak.  this lab still needs work

--- labs/lab15a-files/point.h
+++ labs/lab15a-files/point.h
@@ -1,8 +1,8 @@
 #pragma once
 
+#include <iosfwd>
 #include <string>
 #include <vector>
-
 
 using std::string;
 using std::vector;
@@ -14,10 +14,11 @@ struct Point {
 
 Point         get_next_point (const string& prompt);
 vector<Point> get_points  ();
-void          write_points(const vector<Point> points, const string& fname);
-void          show_points (const vector<Point> points);
+void          write_points(const vector<Point>& points, const string& fname);
 vector<Point> read_points (const string& fname);
+void          show_points(const vector<Point>& points);
 
 std::istream& operator>>(std::istream& is, Point& p);
-
+std::ostream& operator<<(std::ostream& os, const Point& p);
+std::ostream& operator<<(std::ostream& os, const vector<Point>& p);
 
