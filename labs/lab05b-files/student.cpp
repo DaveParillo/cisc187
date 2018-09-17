11cd4f53d30e948888e2c502c2a4d23470573eed
DaveParillo <DaveParillo@users.noreply.github.com>
2018-09-16 21:34:39 -0700

    updated comments

--- labs/lab05b-files/student.cpp
+++ labs/lab05b-files/student.cpp
@@ -8,27 +8,33 @@
 
 
 void add_student(std::vector<student*>* students, std::istringstream& iss) {
-    // Your code here. Implement a function to add one student and their grades from the input stream to the vector
+    // Your code here. 
+    // Implement a function to add one student and their grades from the input stream to the vector
+
 }
 
 std::vector<student*> load_classroom() {
-    std::string input;
+    std::string line;
     // Your code here. Init a new classroom
-    //
-    while (getline(std::cin, input)) {
-        std::istringstream stream(input);
+    
+    while (getline(std::cin, line)) {
+        std::istringstream stream(line);
         // Your code here. add students to the classroom
-        input.clear();
+
+        line.clear();
     }
     return classroom;
 }
 
 std::ostream& operator<< (std::ostream& os, const std::vector<student*>& rhs) {
-    // Your code here. Implement a function to print all students
+    // Your code here.
+    // Implement a function to stream data from a classroom
 }
 
 std::ostream& operator<< (std::ostream& os, const student* rhs) {
-    // Your code here. Implement a function to print one students
+    // Your code here. 
+    // Implement a function to stream data from one student
+    // to the output stream
 }
 
 
