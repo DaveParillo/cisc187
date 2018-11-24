d21db53dc0021077f02e0fe6e92af9ed1a0338b0
DaveParillo <DaveParillo@users.noreply.github.com>
2018-11-24 10:20:11 -0800

    changed indent

--- examples/week13/patterns/chain/Handler.h
+++ examples/week13/patterns/chain/Handler.h
@@ -11,57 +11,63 @@
 // An abstract handler
 // It handles a value, but doesn't specify what is done
 //
+
+
 struct Handler {
-        Handler() = default;
-        virtual ~Handler() = default;
-        virtual void request(int& value) const  = 0;
+  Handler() = default;
+  virtual ~Handler() = default;
+  virtual void request(int& value) const  = 0;
 };
 
 
 // A handler that prints an error if the 
 // error value > error number
 class ErrorHandler : public Handler {
-    int id_ = 1;
-    int error_ = 21;
-    public:
-      ErrorHandler() = default;
-      ErrorHandler(int id, int err_num)
-          : id_{id}, error_{err_num}
-      {}
-      ~ErrorHandler() = default;
-      void request(int& value) const override {
-          if (value > error_) {
-              std::cout << "Error Handler " << id_ << " handled request with error #" << error_ << ".\n";
-          } else {
-              std::cout << "Error Handler " << id_ << " did not handle request.\n";
-          }
-      }
+  private:
+	int id = 1;
+	int error = 21;
+  public:
+	ErrorHandler() = default;
+	ErrorHandler(int id, int err_num)
+	  : id{id}, error{err_num}
+	{}
+	~ErrorHandler() = default;
+	void request(int& value) const override {
+	  if (value > error) {
+		std::cout << "Error Handler " << id 
+		  << " handled request with error #" << error << ".\n";
+	  } else {
+		std::cout << "Error Handler " << id << " did not handle request.\n";
+	  }
+	}
 
 };
 
 // A handler to print the nth fibonacci number
 class FibonacciHandler : public Handler {
-    unsigned fib(unsigned n) const {
-        if(n == 0) return n;
-        unsigned a = 0, b = 1;
-        for (unsigned c = 0, i = 2; i <= n; ++i) {
-            c = a + b;
-            a = b;
-            b = c;
-        }
-        return b;
-    }
-    public:
-    FibonacciHandler() = default;
-    ~FibonacciHandler() = default;
-    void request(int& value) const override {
-        if (value < 999) {
-            value = fib(value);
-            std::cout << "Fibonacci number #" << value << " is " << fib(value) <<  ".\n";
-        } else {
-            std::cout << "Fibonacci Handler did not handle request.\n";
-        }
-    }
+  private:
+	unsigned fib(unsigned n) const {
+	  if(n == 0) return n;
+	  unsigned a = 0, b = 1;
+	  for (unsigned c = 0, i = 2; i <= n; ++i) {
+		c = a + b;
+		a = b;
+		b = c;
+	  }
+	  return b;
+	}
+  public:
+	FibonacciHandler() = default;
+	~FibonacciHandler() = default;
+	void request(int& value) const override {
+	  if (value < 999) {
+		value = fib(value);
+		std::cout << "Fibonacci number #" << value << " is " << fib(value) <<  ".\n";
+	  } else {
+		std::cout << "Fibonacci Handler did not handle request.\n";
+	  }
+	}
 
 };
+
 
