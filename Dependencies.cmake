e1b5858f263744f441c667f12b0085a626d56327
DaveParillo <DaveParillo@users.noreply.github.com>
2025-08-10 10:05:28 -0700

    Initial commit

---
+++ Dependencies.cmake
@@ -0,0 +1,43 @@
+include(cmake/CPM.cmake)
+
+# Done as a function so that updates to variables like
+# CMAKE_CXX_FLAGS don't propagate out to other
+# targets
+function(myproject_setup_dependencies)
+
+  # For each dependency, see if it's
+  # already been provided to us by a parent project
+
+  if(NOT TARGET fmtlib::fmtlib)
+    cpmaddpackage("gh:fmtlib/fmt#11.1.4")
+  endif()
+
+  if(NOT TARGET spdlog::spdlog)
+    cpmaddpackage(
+      NAME
+      spdlog
+      VERSION
+      1.15.2
+      GITHUB_REPOSITORY
+      "gabime/spdlog"
+      OPTIONS
+      "SPDLOG_FMT_EXTERNAL ON")
+  endif()
+
+  if(NOT TARGET Catch2::Catch2WithMain)
+    cpmaddpackage("gh:catchorg/Catch2@3.8.1")
+  endif()
+
+  if(NOT TARGET CLI11::CLI11)
+    cpmaddpackage("gh:CLIUtils/CLI11@2.5.0")
+  endif()
+
+  if(NOT TARGET ftxui::screen)
+    cpmaddpackage("gh:ArthurSonzogni/FTXUI@6.0.2")
+  endif()
+
+  if(NOT TARGET tools::tools)
+    cpmaddpackage("gh:lefticus/tools#update_build_system")
+  endif()
+
+endfunction()
